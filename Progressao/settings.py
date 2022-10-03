5dd8017b4528470378fe16264c311271f4d23322
Ariel <91798390+ArielMcR@users.noreply.github.com>
2022-10-03 17:29:57 -0300

    Update settings.py

--- Progressao/settings.py
+++ Progressao/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j1^qfrbd!s4k$+y+=vasc0zj(rpes(k**i6*m%$b=(^e!io_5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
